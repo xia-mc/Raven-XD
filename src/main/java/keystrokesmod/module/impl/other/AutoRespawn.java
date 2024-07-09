c53573da2b3274f3058577bc180a3da7b5e11561
SleepingPig35 <andyzeng207@gmail.com>
2024-07-09 15:30:49 +0100

    Add files via upload

---
+++ src/main/java/keystrokesmod/module/impl/other/AutoRespawn.java
@@ -0,0 +1,23 @@
+package keystrokesmod.module.impl.other;
+
+import keystrokesmod.event.ReceivePacketEvent;
+import keystrokesmod.module.Module;
+import keystrokesmod.module.setting.impl.DescriptionSetting;
+import keystrokesmod.utility.PacketUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.C16PacketClientStatus;
+import org.jetbrains.annotations.NotNull;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class AutoRespawn extends Module{
+    public AutoRespawn() {
+        super("AutoRespawn", category.other);
+        this.registerSetting(new DescriptionSetting("Automatically respawns you after you die."));
+    }
+   @SubscribeEvent
+    public void onReceive(@NotNull ReceivePacketEvent event) {
+        if(Minecraft.getMinecraft().thePlayer.isDead){
+            PacketUtils.sendPacketNoEvent(new C16PacketClientStatus(C16PacketClientStatus.EnumState.PERFORM_RESPAWN));
+        }
+    };
+}
