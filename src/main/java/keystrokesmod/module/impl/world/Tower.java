936724d72bdda765f1d8ca1062573b64a9ddc2aa
Xiaojie0403 <1924711979@qq.com>
2024-07-15 13:10:39 +0800

    Update Tower.java#2
    第二次，再过不去我就自杀了

--- src/main/java/keystrokesmod/module/impl/world/Tower.java
+++ src/main/java/keystrokesmod/module/impl/world/Tower.java
@@ -1,8 +1,6 @@
 package keystrokesmod.module.impl.world;
 
-import keystrokesmod.Raven;
 import keystrokesmod.event.PreMotionEvent;
-import keystrokesmod.event.PreUpdateEvent;
 import keystrokesmod.event.SendPacketEvent;
 import keystrokesmod.module.Module;
 import keystrokesmod.module.ModuleManager;
@@ -12,22 +10,11 @@ import keystrokesmod.module.setting.impl.DescriptionSetting;
 import keystrokesmod.module.setting.impl.ModeSetting;
 import keystrokesmod.module.setting.impl.SliderSetting;
 import keystrokesmod.module.setting.utils.ModeOnly;
-import keystrokesmod.utility.Reflection;
 import keystrokesmod.utility.Utils;
-import net.minecraft.network.play.client.C03PacketPlayer;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.apache.commons.lang3.tuple.Triple;
 import org.lwjgl.input.Keyboard;
-
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-
-import static keystrokesmod.module.ModuleManager.scaffold;
 
 public class Tower extends Module {
     private final ModeSetting mode;
@@ -41,10 +28,15 @@ public class Tower extends Module {
     private final ButtonSetting hypixelNoStrafe;
     private final SliderSetting hypixelOffGroundSpeed;
     private final ButtonSetting lowHop;
+    private final ButtonSetting lowHop2; // New button setting for lowhop2
     private int slowTicks;
     private boolean wasTowering;
     private int offGroundTicks = 0;
     private BlockPos toweredBlock = null;
+    private int onGroundTicks;
+    private boolean watchdog;
+    private int inAirTicks;
+
     public Tower() {
         super("Tower", category.world);
         this.registerSetting(new DescriptionSetting("Works with SafeWalk & Scaffold"));
@@ -59,10 +51,16 @@ public class Tower extends Module {
         this.registerSetting(hypixelOffGroundSpeed = new SliderSetting("Hypixel off ground speed", 0.5, 0.0, 1.0, 0.01, mode1));
         this.registerSetting(hypixelNoStrafe = new ButtonSetting("Hypixel no strafe", false, mode1));
         this.registerSetting(lowHop = new ButtonSetting("Low hop", false, mode1));
+        this.registerSetting(lowHop2 = new ButtonSetting("Low hop 2", false, mode1)); // Register lowhop2 setting
         this.registerSetting(disableWhileCollided = new ButtonSetting("Disable while collided", false));
         this.registerSetting(disableWhileHurt = new ButtonSetting("Disable while hurt", false));
         this.registerSetting(sprintJumpForward = new ButtonSetting("Sprint jump forward", true));
         this.canBeEnabled = false;
+
+        // Initialize additional fields
+        onGroundTicks = 0;
+        watchdog = false;
+        inAirTicks = 0;
     }
 
     @Override
@@ -70,10 +68,14 @@ public class Tower extends Module {
         wasTowering = false;
         offGroundTicks = 0;
         toweredBlock = null;
+        // Reset additional fields
+        onGroundTicks = 0;
+        watchdog = false;
+        inAirTicks = 0;
     }
 
     @SubscribeEvent
-    public void onPreMotion(PreMotionEvent e) throws IllegalAccessException {
+    public void onPreMotion(PreMotionEvent e) {
         if (canTower()) {
             wasTowering = true;
 
@@ -100,6 +102,7 @@ public class Tower extends Module {
                         mc.thePlayer.motionX *= moveSpeed;
                         mc.thePlayer.motionZ *= moveSpeed;
                     }
+                    break;
                 case 2:
                     if (mc.thePlayer.onGround)
                         mc.thePlayer.motionY = 0.42F;
@@ -111,19 +114,65 @@ public class Tower extends Module {
             if (wasTowering && slowedTicks.getInput() > 0 && modulesEnabled()) {
                 if (slowTicks++ < slowedTicks.getInput()) {
                     Utils.setSpeed(Math.max(slowedSpeed.getInput() * 0.1 - 0.25, 0));
-                }
-                else {
+                } else {
                     slowTicks = 0;
                     wasTowering = false;
                 }
-            }
-            else {
+            } else {
                 if (wasTowering) {
                     wasTowering = false;
                 }
                 slowTicks = 0;
             }
             reset();
+        }
+
+        // Implement low hop logic from provided code
+        if (lowHop.isToggled()) {
+            Entity player = mc.thePlayer;
+            onGroundTicks = player.onGround ? onGroundTicks + 1 : 0;
+            watchdog = (watchdog || onGroundTicks == 1) && onGroundTicks < 2;
+            Vec3 motion = mc.thePlayer.getMotion();
+            if (onGroundTicks > 0) {
+                mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY + 1E-14, mc.thePlayer.posZ);
+            }
+            if (watchdog) {
+                if (motion.y == 0.16477328182606651) {
+                    mc.thePlayer.setMotion(motion.x, 0.14961479459521598, motion.z);
+                }
+                if (motion.y == 0.0682225000311085) {
+                    mc.thePlayer.setMotion(motion.x, 0.0532225003663811, motion.z);
+                }
+                if (motion.y == -0.0262419501516868) {
+                    mc.thePlayer.setMotion(motion.x, -0.027141950136226, motion.z);
+                }
+                if (motion.y == -0.104999113177072) {
+                    mc.thePlayer.setMotion(motion.x, -0.31999911675335113, motion.z);
+                }
+                if (motion.y == -0.3919991420476618) {
+                    mc.thePlayer.setMotion(motion.x, -0.3968991421057737, motion.z);
+                }
+            }
+        }
+
+        // Implement low hop 2 logic
+        if (lowHop2.isToggled() && ModuleManager.scaffold.isEnabled() && Keyboard.isKeyDown(57)) {
+            Entity player = mc.thePlayer;
+            inAirTicks = player.onGround ? 0 : inAirTicks + 1;
+            Vec3 motion = mc.thePlayer.getMotion();
+            if (player.onGround) {
+                mc.thePlayer.setMotion(motion.x, 0.4191, motion.z);
+            } else {
+                if (inAirTicks == 1) {
+                    mc.thePlayer.setMotion(motion.x, 0.327318, motion.z);
+                }
+                if (inAirTicks == 5) {
+                    mc.thePlayer.setMotion(motion.x, -0.005, motion.z);
+                }
+                if (inAirTicks == 6) {
+                    mc.thePlayer.setMotion(motion.x, -1.0, motion.z);
+                }
+            }
         }
     }
 
@@ -132,71 +181,22 @@ public class Tower extends Module {
         if (canTower() && (int) mode.getInput() == 2) {
             if (mc.thePlayer.motionY > -0.0784000015258789 && event.getPacket() instanceof C08PacketPlayerBlockPlacement) {
                 final C08PacketPlayerBlockPlacement wrapper = ((C08PacketPlayerBlockPlacement) event.getPacket());
-
-                if (wrapper.getPosition().equals(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY - 1.4, mc.thePlayer.posZ))) {
+                BlockPos pos = wrapper.getPosition();
+                if (pos.equals(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY - 1.4, mc.thePlayer.posZ))) {
                     mc.thePlayer.motionY = -0.0784000015258789;
                 }
             }
         }
     }
 
-    @SubscribeEvent
-    public void onPreUpdate(PreUpdateEvent event) {
-        if (mc.thePlayer.onGround) {
-            offGroundTicks = 0;
-        } else {
-            offGroundTicks++;
-        }
-
-        if (canTower() && mode.getInput() == 1 && lowHop.isToggled() && Utils.isMoving()) {
-            switch (offGroundTicks) {
-                case 0:
-                    mc.thePlayer.motionY = 0.4196;
-                    break;
-                case 3:
-                case 4:
-                    mc.thePlayer.motionY = 0;
-                    break;
-                case 5:
-                    mc.thePlayer.motionY = 0.4191;
-                    break;
-                case 6:
-                    mc.thePlayer.motionY = 0.3275;
-                    break;
-                case 11:
-                    mc.thePlayer.motionY = -0.5;
-                    break;
-            }
-        }
-    }
-
     private void reset() {
+        // Add reset logic here if needed
     }
 
     private boolean canTower() {
-        if (scaffold.totalBlocks() == 0) return false;
-        if (mc.currentScreen != null) return false;
-        if (!Utils.nullCheck() || !Utils.jumpDown()) {
-            return false;
-        }
-        else if (disableWhileHurt.isToggled() && mc.thePlayer.hurtTime >= 9) {
-            return false;
-        }
-        else if (disableWhileCollided.isToggled() && mc.thePlayer.isCollidedHorizontally) {
-            return false;
-        }
-        else return modulesEnabled();
+    return ModuleManager.scaffold.isEnabled() && ModuleManager.scaffold.tower.isToggled() &&
+            mc.currentScreen == null && !Utils.nullCheck() && Utils.jumpDown() &&
+            !(disableWhileHurt.isToggled() && mc.thePlayer.hurtTime >= 9) &&
+            !(disableWhileCollided.isToggled() && mc.thePlayer.isCollidedHorizontally) &&
+            modulesEnabled();
     }
-
-    private boolean modulesEnabled() {
-        return  ((ModuleManager.safeWalk.isEnabled() && ModuleManager.safeWalk.tower.isToggled() && SafeWalk.canSafeWalk()) || (scaffold.isEnabled() && scaffold.tower.isToggled()));
-    }
-
-    public boolean canSprint() {
-        return canTower() && this.sprintJumpForward.isToggled() && Keyboard.isKeyDown(mc.gameSettings.keyBindForward.getKeyCode()) && Utils.jumpDown();
-    }
-
-    private boolean diagonal() {
-        return (Math.abs(mc.thePlayer.motionX) > 0.05 && Math.abs(mc.thePlayer.motionZ) > 0.05);
-    }
-}
