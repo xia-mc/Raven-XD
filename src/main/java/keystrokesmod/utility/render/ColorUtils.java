ac8d071bae895b8084e513ff9ad327f7be3717bb
TejasLamba2006 <tejas22feb@gmail.com>
2024-07-18 14:36:09 +0530

    fix

--- src/main/java/keystrokesmod/utility/render/ColorUtils.java
+++ src/main/java/keystrokesmod/utility/render/ColorUtils.java
@@ -3,10 +3,18 @@ package keystrokesmod.utility.render;
 import net.minecraft.client.renderer.GlStateManager;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class ColorUtils {
-    public static final List<AnimationUtils> animation = new ArrayList<>(Collections.nCopies(18, new AnimationUtils(0.0F)));
+    public static final List<AnimationUtils> animation = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 18; i++) {
+            animation.add(new AnimationUtils(0.0F));
+        }
+    }
     // fixing this never >:(
     private static final Pattern COLOR_PATTERN = Pattern.compile("(?i)§[0-9A-FK-OR]");
 
@@ -24,26 +32,25 @@ public class ColorUtils {
                 return new Color(255, 0, 255);
         }
     }
-    }
 
     public static Color getFontColor(int id, int alpha) {
         Color rawColor = getRawFontColor(id);
         int speed = 12;
 
         if (id == 1) {
-            animation[12].setAnimation(rawColor.getRed(), speed);
-            animation[13].setAnimation(rawColor.getGreen(), speed);
-            animation[14].setAnimation(rawColor.getBlue(), speed);
+            animation.get(12).setAnimation(rawColor.getRed(), speed);
+            animation.get(13).setAnimation(rawColor.getGreen(), speed);
+            animation.get(14).setAnimation(rawColor.getBlue(), speed);
 
-            return new Color((int) animation[12].getValue(), (int) animation[13].getValue(), (int) animation[14].getValue(), alpha);
+            return new Color((int) animation.get(12).getValue(), (int) animation.get(13).getValue(), (int) animation.get(14).getValue(), alpha);
         }
 
         if (id == 2) {
-            animation[15].setAnimation(rawColor.getRed(), speed);
-            animation[16].setAnimation(rawColor.getGreen(), speed);
-            animation[17].setAnimation(rawColor.getBlue(), speed);
+            animation.get(15).setAnimation(rawColor.getRed(), speed);
+            animation.get(16).setAnimation(rawColor.getGreen(), speed);
+            animation.get(17).setAnimation(rawColor.getBlue(), speed);
 
-            return new Color((int) animation[15].getValue(), (int) animation[16].getValue(), (int) animation[17].getValue(), alpha);
+            return new Color((int) animation.get(15).getValue(), (int) animation.get(16).getValue(), (int) animation.get(17).getValue(), alpha);
         }
 
         return rawColor;
