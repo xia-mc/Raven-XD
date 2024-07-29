xia-mc/Raven-XD

commit c0b99074e289f06fb655779327bd408df39170bf

Add AutoRegister Add Hypixel mode to Disabler     - InvMove Remove Reduce Remove NyaProxy Add Instant mode to InfiniteAura Add Open folder button to CustomCape Fix AutoPot will crash game on death Add Pulse to Blink Add Cancel crash to ExploitFixer Fix ModeValue do wrong load/save.

src/main/java/keystrokesmod/clickgui/components/impl/ModuleComponent.java
@@ -129,7 +129,7 @@ public class ModuleComponent implements IComponent {
         if (this.po && !this.settings.isEmpty()) {
             for (Component c : this.settings) {
                 Setting setting = c.getSetting();
-                if (setting == null || setting.isVisible()) {
+                if (setting != null && setting.isVisible()) {
                     c.render();
                 }
             }

src/main/java/keystrokesmod/event/ScaffoldPlaceEvent.java
@@ -1,0 +1,32 @@
+package keystrokesmod.event;
+
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+@Cancelable
+public class ScaffoldPlaceEvent extends Event {
+    private MovingObjectPosition hitResult;
+    private boolean extra;
+
+    public ScaffoldPlaceEvent(MovingObjectPosition hitResult, boolean extra) {
+        this.hitResult = hitResult;
+        this.extra = extra;
+    }
+
+    public MovingObjectPosition getHitResult() {
+        return hitResult;
+    }
+
+    public void setHitResult(MovingObjectPosition hitResult) {
+        this.hitResult = hitResult;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+    public void setExtra(boolean extra) {
+        this.extra = extra;
+    }
+}

src/main/java/keystrokesmod/mixins/impl/client/MixinMinecraft.java
@@ -4,13 +4,16 @@ import keystrokesmod.event.ClickEvent;
 import keystrokesmod.event.PreTickEvent;
 import keystrokesmod.event.RightClickEvent;
 import keystrokesmod.module.ModuleManager;
+import keystrokesmod.module.impl.client.Notifications;
 import keystrokesmod.module.impl.combat.HitBox;
 import keystrokesmod.module.impl.combat.Reach;
+import keystrokesmod.module.impl.exploit.ExploitFixer;
 import keystrokesmod.module.impl.render.Animations;
 import keystrokesmod.module.impl.render.FreeLook;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.GameSettings;
+import net.minecraft.crash.CrashReport;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
@@ -79,5 +82,15 @@ public abstract class MixinMinecraft {
         MinecraftForge.EVENT_BUS.post(event);
         if (event.isCanceled())
             ci.cancel();
+    }
+
+    @Inject(method = "crashed", at = @At("HEAD"), cancellable = true)
+    private void onCrashed(CrashReport crashReport, CallbackInfo ci) {
+        try {
+            if (ExploitFixer.onCrash(crashReport)) {
+                ci.cancel();
+            }
+        } catch (Throwable ignored) {
+        }
     }
 }

src/main/java/keystrokesmod/mixins/impl/entity/MixinEntityPlayer.java
@@ -1,7 +1,6 @@
 package keystrokesmod.mixins.impl.entity;
 
 import keystrokesmod.module.ModuleManager;
-import keystrokesmod.module.impl.combat.Reduce;
 import keystrokesmod.module.impl.combat.Velocity;
 import keystrokesmod.module.impl.movement.KeepSprint;
 import keystrokesmod.module.impl.render.Particles;
@@ -102,10 +101,7 @@ public abstract class MixinEntityPlayer extends EntityLivingBase {
                     if (flag2) {
                         if (i > 0) {
                             p_attackTargetEntityWithCurrentItem_1_.addVelocity((double) (-MathHelper.sin(this.rotationYaw * 3.1415927F / 180.0F) * (float) i * 0.5F), 0.1, (double) (MathHelper.cos(this.rotationYaw * 3.1415927F / 180.0F) * (float) i * 0.5F));
-                            if (ModuleManager.reduce != null && ModuleManager.reduce.isEnabled()) {
-                                Reduce.reduce(p_attackTargetEntityWithCurrentItem_1_);
-                            }
-                            else if (ModuleManager.keepSprint != null && ModuleManager.keepSprint.isEnabled()) {
+                            if (ModuleManager.keepSprint != null && ModuleManager.keepSprint.isEnabled()) {
                                 KeepSprint.keepSprint(p_attackTargetEntityWithCurrentItem_1_);
                             }
 //                            else if (ModuleManager.hitSelect != null && ModuleManager.hitSelect.isEnabled()) {

src/main/java/keystrokesmod/mixins/impl/render/RenderManagerAccessor.java
@@ -13,6 +13,9 @@ public interface RenderManagerAccessor {
     @Accessor("renderPosY")
     double getRenderPosY();
 
+    @Accessor("renderPosY")
+    void setRenderPosY(double renderPosY);
+
     @Accessor("renderPosZ")
     double getRenderPosZ();
 }

src/main/java/keystrokesmod/mixins/impl/world/MixinBlock.java
@@ -28,7 +28,7 @@ public abstract class MixinBlock {
      *
      */
     @Inject(method = "addCollisionBoxesToList", at = @At("HEAD"), cancellable = true)
-    public void addCollisionBoxesToList(World worldIn, BlockPos pos, IBlockState state, AxisAlignedBB mask, List<AxisAlignedBB> list,
+    public void onAddCollisionBoxesToList(World worldIn, BlockPos pos, IBlockState state, AxisAlignedBB mask, List<AxisAlignedBB> list,
                                         Entity collidingEntity, CallbackInfo ci) {
         if (collidingEntity == Raven.mc.thePlayer) {
             ci.cancel();

src/main/java/keystrokesmod/module/ModuleManager.java
@@ -44,7 +44,6 @@ public class ModuleManager {
     public static Potions potions;
     public static NoFall noFall;
     public static PlayerESP playerESP;
-    public static Reduce reduce;
     public static SafeWalk safeWalk;
     public static KeepSprint keepSprint;
     public static Velocity velocity;
@@ -101,6 +100,7 @@ public class ModuleManager {
     public static WallClimb wallClimb;
     public static Jesus jesus;
     public static ExploitFixer exploitFixer;
+    public static AutoRegister autoRegister;
 
     public void register() {
 
@@ -129,7 +129,6 @@ public class ModuleManager {
         this.addModule(killAuraV2 = new KillAuraV2());
         this.addModule(moreKB = new MoreKB());
         this.addModule(reach = new Reach());
-        this.addModule(reduce = new Reduce());
         this.addModule(new RodAimbot());
         this.addModule(timerRange = new TimerRange());
         this.addModule(velocity = new Velocity());
@@ -180,6 +179,7 @@ public class ModuleManager {
         this.addModule(slotHandler = new SlotHandler());
         this.addModule(staffDetector = new StaffDetector());
         this.addModule(new BedProximityAlert());
+        this.addModule(autoRegister = new AutoRegister());
 
         // player
         this.addModule(new AntiAFK());

src/main/java/keystrokesmod/module/impl/client/NyaProxy.java
@@ -1,166 +1,0 @@
-package keystrokesmod.module.impl.client;
-
-import com.mojang.realmsclient.gui.ChatFormatting;
-import keystrokesmod.Raven;
-import keystrokesmod.event.PreConnectEvent;
-import keystrokesmod.mixins.impl.client.GuiConnectingAccessor;
-import keystrokesmod.module.Module;
-import keystrokesmod.module.setting.impl.ButtonSetting;
-import keystrokesmod.module.setting.impl.DescriptionSetting;
-import keystrokesmod.module.setting.impl.SliderSetting;
-import keystrokesmod.utility.GuiConnectingMsg;
-import keystrokesmod.utility.Utils;
-import net.minecraft.client.gui.GuiDisconnected;
-import net.minecraft.client.network.NetHandlerLoginClient;
-import net.minecraft.network.EnumConnectionState;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.handshake.client.C00Handshake;
-import net.minecraft.network.login.client.C00PacketLoginStart;
-import net.minecraft.util.ChatComponentTranslation;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.jetbrains.annotations.NotNull;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-public class NyaProxy extends Module {
-    private final SliderSetting maxRetryTimes;
-    private final ButtonSetting autoFlushDns;
-    private GuiConnectingMsg.Data guiData = new GuiConnectingMsg.Data();
-    private short retry = 0;
-
-    public NyaProxy() {
-        super("NyaProxy", category.client);
-        this.registerSetting(new DescriptionSetting("work with NyaProxy server."));
-        this.registerSetting(maxRetryTimes = new SliderSetting("Max retry times", 20, 0, 50, 1));
-        this.registerSetting(autoFlushDns = new ButtonSetting("Auto flush dns (Windows)", false));
-    }
-
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void onConnect(@NotNull PreConnectEvent event) {
-        NYA_BACKENDS backend;
-        try {
-            backend = isNyaProxy(event.getIp()).orElseThrow(RuntimeException::new);
-        } catch (RuntimeException e) {
-            return;
-        }
-
-        event.setCanceled(true);
-        retry = 0;
-        guiData = new GuiConnectingMsg.Data();
-
-        guiData.set(ChatFormatting.GRAY + "[" + ChatFormatting.AQUA + "NyaProxy" + ChatFormatting.GRAY + "] " + ChatFormatting.BLUE);
-        switch (backend) {
-            case SUQIAN1:
-                guiData.append("宿迁节点1");
-                break;
-            case SUQIAN2:
-                guiData.append("宿迁节点2");
-                break;
-            case SHAOXING:
-                guiData.append("绍兴节点");
-                break;
-        }
-
-        event.getExtraMessage().update(guiData);
-        Raven.getExecutor().execute(() -> connectWithRetry(event));
-    }
-
-    private void connectWithRetry(@NotNull PreConnectEvent event) {
-        GuiConnectingAccessor guiConnecting = (GuiConnectingAccessor) event.getScreen();
-
-        InetAddress inetaddress = null;
-
-        try {
-            if (guiConnecting.isCancel()) {
-                return;
-            }
-
-            if (autoFlushDns.isToggled()) {
-                Runtime.getRuntime().exec("ipConfig /flushDns");
-            }
-
-            inetaddress = InetAddress.getByName(event.getIp());
-            guiConnecting.setNetworkManager(NetworkManager.func_181124_a(inetaddress, event.getPort(), mc.gameSettings.func_181148_f()));
-            guiConnecting.getNetworkManager().setNetHandler(new NetHandlerLoginClient(guiConnecting.getNetworkManager(), mc, guiConnecting.getPreviousGuiScreen()));
-            guiConnecting.getNetworkManager().sendPacket(new C00Handshake(47, event.getIp(), event.getPort(), EnumConnectionState.LOGIN, true));
-            guiConnecting.getNetworkManager().sendPacket(new C00PacketLoginStart(mc.getSession().getProfile()));
-        } catch (UnknownHostException var5) {
-            if (guiConnecting.isCancel()) {
-                return;
-            }
-
-            guiConnecting.getLogger().error("Couldn't connect to server", var5);
-            mc.displayGuiScreen(new GuiDisconnected(guiConnecting.getPreviousGuiScreen(), "connect.failed", new ChatComponentTranslation("disconnect.genericReason", "Unknown host")));
-        } catch (Exception var6) {
-            if (guiConnecting.isCancel()) {
-                return;
-            }
-
-            guiConnecting.getLogger().error("Couldn't connect to server", var6);
-            String s = var6.toString();
-            if (inetaddress != null) {
-                String s1 = inetaddress + ":" + event.getPort();
-                s = s.replaceAll(s1, "");
-            }
-
-            String unformatted = Utils.getUnformatedString(s);
-            if (retry >= maxRetryTimes.getInput() || unformatted.startsWith("You are temporarily banned")) {
-                guiData = null;
-                retry = 0;
-                mc.displayGuiScreen(new GuiDisconnected(guiConnecting.getPreviousGuiScreen(), "connect.failed", new ChatComponentTranslation("disconnect.genericReason", s)));
-            } else {
-                guiData.set("§c§l第§r§l " + (retry + 1) + " §c§l次失败§7: " +
-                        NYA_EXCEPTIONS.entrySet().stream()
-                                .filter(entry -> unformatted.startsWith(entry.getKey()))
-                                .map(Map.Entry::getValue)
-                                .findAny()
-                                .orElse("§r" + s)
-                );
-                event.getExtraMessage().update(guiData);
-
-                retry++;
-                connectWithRetry(event);
-            }
-        }
-    }
-
-    public static Optional<NYA_BACKENDS> isNyaProxy(@NotNull String ipAddress) {
-        final String formattedIp = ipAddress.split(":")[0];
-
-        for (NYA_BACKENDS backend : NYA_BACKENDS.values()) {
-            if (formattedIp.endsWith(backend.getIp())) {
-                return Optional.of(backend);
-            }
-        }
-        return Optional.empty();
-    }
-
-    public enum NYA_BACKENDS {
-        SUQIAN1(".suqian1.connect.hoursly.nyaproxy.xyz"),
-        SUQIAN2(".suqian2.connect.hoursly.nyaproxy.xyz"),
-        SHAOXING(".shaoxing.connect.hoursly.nyaproxy.xyz");
-
-        final String ip;
-
-        NYA_BACKENDS(String ip) {
-            this.ip = ip;
-        }
-
-        public String getIp() {
-            return ip;
-        }
-    }
-
-    public static final Map<String, String> NYA_EXCEPTIONS = new HashMap<>();
-
-    static {
-        NYA_EXCEPTIONS.put("java.net.ConnectException: Connection refused: no further information: ", "服务不可用。");
-//        NYA_EXCEPTIONS.put("java.net.ConnectException: Connection refused: 远程主机强迫关闭了一个现有的连接", "IP被NyaProxy暂时屏蔽。");
-        NYA_EXCEPTIONS.put("ZBProxy - Connection Rejected", "未开启计费/服务正在初始化。");
-    }
-}

commit 5a4a277eca497fa52bf263f5565745fef0a47030

Fixes a bug caused by commit `c0b9907`

src/main/java/keystrokesmod/clickgui/components/impl/ModuleComponent.java
@@ -129,7 +129,7 @@ public class ModuleComponent implements IComponent {
         if (this.po && !this.settings.isEmpty()) {
             for (Component c : this.settings) {
                 Setting setting = c.getSetting();
-                if (setting != null && setting.isVisible()) {
+                if (setting == null || setting.isVisible()) {
                     c.render();
                 }
             }