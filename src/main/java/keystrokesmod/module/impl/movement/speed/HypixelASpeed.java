a34dc7f7b9eaf69fd68e6a16aa3b050e681f8786
xia-mc <2052472631@qq.com>
2024-08-02 19:32:03 +0800

    update version to 1.21.0 fix Hypixel A speed

--- src/main/java/keystrokesmod/module/impl/movement/speed/HypixelASpeed.java
+++ src/main/java/keystrokesmod/module/impl/movement/speed/HypixelASpeed.java
@@ -2,7 +2,6 @@ package keystrokesmod.module.impl.movement.speed;
 
 import keystrokesmod.event.PreMotionEvent;
 import keystrokesmod.module.impl.movement.Speed;
-import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.module.setting.impl.SubMode;
 import keystrokesmod.utility.MoveUtil;
 import keystrokesmod.utility.Utils;
@@ -10,21 +9,20 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class HypixelASpeed extends SubMode<Speed> {
-    private final ButtonSetting autoJump;
 
     public HypixelASpeed(String name, @NotNull Speed parent) {
         super(name, parent);
-        this.registerSetting(autoJump = new ButtonSetting("Auto jump", true));
     }
 
     @SubscribeEvent
     public void onPreMotion(PreMotionEvent event) {
         if (parent.noAction()) return;
 
-        if (mc.thePlayer.onGround) {
-            if (!Utils.jumpDown() && autoJump.isToggled())
-                mc.thePlayer.jump();
-            MoveUtil.strafe(MoveUtil.getAllowedHorizontalDistance());
+        if (mc.thePlayer.onGround && MoveUtil.isMoving()) {
+            if (!Utils.jumpDown()) {
+                mc.thePlayer.motionY = 0.42;
+                MoveUtil.strafe(0.415);
+            }
         }
     }
 }
