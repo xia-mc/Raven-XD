7b1f92176aafbdb07949e1a8728b633eb790813d
xia-mc <2052472631@qq.com>
2024-08-05 12:25:48 +0800

    fix

--- src/main/java/keystrokesmod/utility/Commands.java
+++ src/main/java/keystrokesmod/utility/Commands.java
@@ -155,47 +155,6 @@ public class Commands {
                     }
 
                 });
-            } else if (firstArg.equals("setvelocity")) {
-                if (!hasArgs) {
-                    print(invSyn, 1);
-                    return;
-                }
-
-                if (args.size() != 3) {
-                    print(invSyn, 1);
-                    return;
-                }
-
-                double value;
-
-                try {
-                    value = Double.parseDouble(args.get(2));
-                } catch (Exception e) {
-                    print("&cInvalid value. [-100 - 300)", 1);
-                    return;
-                }
-
-                if (value > 300 || value < -100) {
-                    print("&cInvalid value. [-100 - 300)", 1);
-                    return;
-                }
-
-                switch (args.get(1)) {
-                    case "horizontal":
-                    case "h":
-                        Velocity.horizontal.setValueRaw(value);
-                        break;
-                    case "vertical":
-                    case "v":
-                        Velocity.vertical.setValueRaw(value);
-                        break;
-                    default:
-                        print(invSyn, 1);
-                        return;
-                }
-
-                print("&aSet " + args.get(1) + " velocity to ", 1);
-                print(args.get(2), 0);
             } else if (firstArg.equals("ping")) {
                 Ping.checkPing();
             } else if (firstArg.equals("clear")) {
@@ -482,7 +441,6 @@ public class Commands {
                 print("&eModule-specific:", 0);
                 print("1 cname [name]", 0);
                 print("2 " + FakeChat.command + " [msg]", 0);
-                print("3 setvelocity [h/v] [value]", 0);
                 print("4 killmessage [message]", 0);
                 print(String.format("5 clientname [name (current is '%s')]", Watermark.customName), 0);
             }
