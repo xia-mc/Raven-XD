6efa3fd585ad6d68d9f5ef1ea94d08bc14918edd
xia__mc <2052472631@qq.com>
2024-08-14 16:50:40 +0800

    fix HypixelLowHopSpeed

--- src/main/java/keystrokesmod/module/impl/movement/speed/hypixel/HypixelLowHopSpeed.java
+++ src/main/java/keystrokesmod/module/impl/movement/speed/hypixel/HypixelLowHopSpeed.java
@@ -30,7 +30,7 @@ public class HypixelLowHopSpeed extends SubMode<HypixelSpeed> {
 
     @SubscribeEvent
     public void onPrePlayerInput(PrePlayerInputEvent event) {
-        if (!MoveUtil.isMoving() || !HypixelMotionDisabler.isDisabled()) return;
+        if (!MoveUtil.isMoving() || !HypixelMotionDisabler.isDisabled() || parent.parent.noAction()) return;
         switch (parent.parent.offGroundTicks) {
             case 0:
                 if (!Utils.jumpDown()) {
