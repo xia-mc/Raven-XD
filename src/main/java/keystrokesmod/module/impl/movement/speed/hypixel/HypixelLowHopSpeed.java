6048a96c35cb32f4bed079dfbe71671aa4cda45b
xia__mc <2052472631@qq.com>
2024-08-19 17:05:02 +0800

    Improve HypixelLowHopSpeed

--- src/main/java/keystrokesmod/module/impl/movement/speed/hypixel/HypixelLowHopSpeed.java
+++ src/main/java/keystrokesmod/module/impl/movement/speed/hypixel/HypixelLowHopSpeed.java
@@ -4,6 +4,8 @@ import keystrokesmod.event.PrePlayerInputEvent;
 import keystrokesmod.module.impl.exploit.disabler.hypixel.HypixelMotionDisabler;
 import keystrokesmod.module.impl.movement.speed.HypixelSpeed;
 import keystrokesmod.module.setting.impl.ButtonSetting;
+import keystrokesmod.module.setting.impl.DescriptionSetting;
+import keystrokesmod.module.setting.impl.ModeSetting;
 import keystrokesmod.module.setting.impl.SubMode;
 import keystrokesmod.utility.MoveUtil;
 import keystrokesmod.utility.Utils;
@@ -14,18 +16,12 @@ import org.jetbrains.annotations.NotNull;
  * Vulcan speed lol
  */
 public class HypixelLowHopSpeed extends SubMode<HypixelSpeed> {
-    private final ButtonSetting strafe;
-
-    private float angle = 0;
+    private final ButtonSetting fast;
 
     public HypixelLowHopSpeed(String name, @NotNull HypixelSpeed parent) {
         super(name, parent);
-        this.registerSetting(strafe = new ButtonSetting("Strafe", false));
-    }
-
-    @Override
-    public void onEnable() {
-        angle = (float) Math.toDegrees(MoveUtil.direction());
+        this.registerSetting(new DescriptionSetting("Motion disabler only."));
+        this.registerSetting(fast = new ButtonSetting("Fast", false));
     }
 
     @SubscribeEvent
@@ -35,18 +31,15 @@ public class HypixelLowHopSpeed extends SubMode<HypixelSpeed> {
             case 0:
                 if (!Utils.jumpDown()) {
                     mc.thePlayer.jump();
-                    MoveUtil.strafe(0.485);
+                    if (fast.isToggled()) {
+                        MoveUtil.strafe(0.485);
+                    } else {
+                        MoveUtil.strafe(MoveUtil.getAllowedHorizontalDistance() - Math.random() / 100f);
+                    }
                 }
                 break;
             case 5:
-                if (strafe.isToggled())
-                    MoveUtil.strafe(0.315);
                 mc.thePlayer.motionY = MoveUtil.predictedMotion(mc.thePlayer.motionY, 2);
-                angle = (float) Math.toDegrees(MoveUtil.direction());
-                break;
-            case 6:
-                if (strafe.isToggled())
-                    MoveUtil.strafe();
                 break;
         }
     }
