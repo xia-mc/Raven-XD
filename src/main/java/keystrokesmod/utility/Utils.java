7c82fdeb238c1fe1d7166c92a68ec518c7909155
xia__mc <2052472631@qq.com>
2024-09-15 23:35:59 +0800

    Add RemoteShop Add BlocksMC mode to Disabler Add ascii fixer Add Strafe mode to Speed

--- src/main/java/keystrokesmod/utility/Utils.java
+++ src/main/java/keystrokesmod/utility/Utils.java
@@ -1052,8 +1052,8 @@ public class Utils {
     public static void inventoryClick(@NotNull GuiScreen s) {
         final ScaledResolution sr = new ScaledResolution(mc);
 
-        int x = Mouse.getX() * s.width / sr.getScaledWidth();
-        int y = s.height - Mouse.getY() * s.height / sr.getScaledHeight() - 1;
+        int x = Mouse.getX() / sr.getScaledWidth() * s.width;
+        int y = s.height - Mouse.getY() / sr.getScaledHeight() * s.height;
 
         ClickEvent event = new ClickEvent();
         MinecraftForge.EVENT_BUS.post(event);
