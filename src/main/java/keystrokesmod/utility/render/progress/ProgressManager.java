8c460049daf92b9b671a8a8c49b40b6a034b4030
xia__mc <2052472631@qq.com>
2024-10-01 12:52:49 +0800

    Fix JumpDSprint mode Scaffold

--- src/main/java/keystrokesmod/utility/render/progress/ProgressManager.java
+++ src/main/java/keystrokesmod/utility/render/progress/ProgressManager.java
@@ -1,28 +1,30 @@
 package keystrokesmod.utility.render.progress;
 
+import cn.hutool.core.collection.ConcurrentHashSet;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Set;
 
 public class ProgressManager {
-    private static final Queue<Progress> progresses = new ConcurrentLinkedQueue<>();
+    private static final Set<Progress> progresses = new ConcurrentHashSet<>();
 
     public static void add(@NotNull Progress progress) {
-        progress.setPosY(progresses.size());
-        progresses.add(progress);
+        if (progresses.add(progress)) {
+            progress.setPosY(progresses.size());
+        }
     }
 
     public static void remove(@NotNull Progress progress) {
-        progresses.remove(progress);
-        int posY = progress.getPosY();
-        progress.setPosY(0);
+        if (progresses.remove(progress)) {
+            int posY = progress.getPosY();
+            progress.setPosY(0);
 
-        for (Progress p : progresses) {
-            if (p.getPosY() > posY)
-                p.setPosY(p.getPosY() - 1);
+            for (Progress p : progresses) {
+                if (p.getPosY() > posY)
+                    p.setPosY(p.getPosY() - 1);
+            }
         }
     }
 
