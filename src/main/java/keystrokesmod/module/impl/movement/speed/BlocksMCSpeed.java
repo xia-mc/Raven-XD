fd5e99a330c97442ae8b1c2c5b93865991fbc447
xia__mc <2052472631@qq.com>
2024-10-23 18:50:04 +0800

    Recode BlocksMCSpeed

--- src/main/java/keystrokesmod/module/impl/movement/speed/BlocksMCSpeed.java
+++ src/main/java/keystrokesmod/module/impl/movement/speed/BlocksMCSpeed.java
@@ -1,79 +1,32 @@
 package keystrokesmod.module.impl.movement.speed;
 
-import keystrokesmod.event.PreMotionEvent;
-import keystrokesmod.event.PrePlayerInputEvent;
-import keystrokesmod.event.ReceivePacketEvent;
+import keystrokesmod.event.PreUpdateEvent;
 import keystrokesmod.module.impl.movement.Speed;
 import keystrokesmod.module.setting.impl.SubMode;
 import keystrokesmod.utility.MoveUtil;
-import net.minecraft.network.play.server.S08PacketPlayerPosLook;
-import net.minecraft.potion.Potion;
+import keystrokesmod.utility.Utils;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class BlocksMCSpeed extends SubMode<Speed> {
-    private boolean reset;
-    private double speed;
-
     public BlocksMCSpeed(String name, @NotNull Speed parent) {
         super(name, parent);
     }
 
     @SubscribeEvent
-    public void onPreMotion(PreMotionEvent event) {
-        if (parent.noAction()) return;
-        if (!MoveUtil.isMoving()) {
-            event.setPosX(event.getPosX() + (Math.random() - 0.5) / 3);
-            event.setPosZ(event.getPosZ() + (Math.random() - 0.5) / 3);
-        }
-    }
+    public void onPreUpdate(@NotNull PreUpdateEvent event) {
+        if (parent.noAction() || !MoveUtil.isMoving()) return;
 
-    @SubscribeEvent
-    public void onReceivePacket(@NotNull ReceivePacketEvent event) {
-        if (event.getPacket() instanceof S08PacketPlayerPosLook) {
-            speed = 0;
-        }
-    }
-
-    @SubscribeEvent
-    public void onPrePlayerInput(PrePlayerInputEvent event) {
-        if (parent.noAction()) return;
-        final double base = MoveUtil.getAllowedHorizontalDistance();
-        final boolean potionActive = mc.thePlayer.isPotionActive(Potion.moveSpeed);
-
-        if (MoveUtil.isMoving()) {
-            switch (parent.offGroundTicks) {
-                case 0:
-                    mc.thePlayer.motionY = MoveUtil.jumpBoostMotion(0.42f);
-                    speed = base * (potionActive ? 1.4 : 2.15);
-                    break;
-
-                case 1:
-                    speed -= 0.8 * (speed - base);
-                    break;
-
-                default:
-                    speed -= speed / MoveUtil.BUNNY_FRICTION;
-                    break;
-            }
-
-            reset = false;
-        } else if (!reset) {
-            speed = 0;
-
-            reset = true;
-            speed = MoveUtil.getAllowedHorizontalDistance();
+        if (mc.thePlayer.onGround) {
+            if (!Utils.jumpDown())
+                mc.thePlayer.jump();
+            MoveUtil.strafe(MoveUtil.getAllowedHorizontalDistance());
+        } else {
+            MoveUtil.strafe();
         }
 
-        if (mc.thePlayer.isCollidedHorizontally) {
-            speed = MoveUtil.getAllowedHorizontalDistance();
+        if (parent.offGroundTicks == 5) {
+            mc.thePlayer.motionY = MoveUtil.predictedMotion(mc.thePlayer.motionY, 2);
         }
-
-        event.setSpeed(Math.max(speed, base), Math.random() / 2000);
-    }
-
-    @Override
-    public void onDisable() {
-        speed = 0;
     }
 }
