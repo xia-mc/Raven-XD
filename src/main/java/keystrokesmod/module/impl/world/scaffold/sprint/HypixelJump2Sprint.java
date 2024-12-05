982192e8b17d1b2235c72f89536820657cbb922e
xia__mc <2052472631@qq.com>
2024-12-05 15:47:38 +0800

    Revert HypixelJump2Sprint lowhop

--- src/main/java/keystrokesmod/module/impl/world/scaffold/sprint/HypixelJump2Sprint.java
+++ src/main/java/keystrokesmod/module/impl/world/scaffold/sprint/HypixelJump2Sprint.java
@@ -3,18 +3,14 @@ package keystrokesmod.module.impl.world.scaffold.sprint;
 import keystrokesmod.event.PrePlayerInputEvent;
 import keystrokesmod.module.ModuleManager;
 import keystrokesmod.module.impl.world.Scaffold;
-import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.utility.MoveUtil;
 import keystrokesmod.utility.Utils;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class HypixelJump2Sprint extends VanillaSprint {
-    private final ButtonSetting lowHop;
-
     public HypixelJump2Sprint(String name, @NotNull Scaffold parent) {
         super(name, parent);
-        this.registerSetting(lowHop = new ButtonSetting("Low hop", true));
     }
 
     @SubscribeEvent
@@ -24,20 +20,6 @@ public class HypixelJump2Sprint extends VanillaSprint {
             if (!Utils.jumpDown()) {
                 MoveUtil.strafe(Math.min(MoveUtil.getAllowedHorizontalDistance(), MoveUtil.speed() * 2) - Math.random() / 100f);
                 mc.thePlayer.jump();
-            }
-        }
-
-        if (lowHop.isToggled() && MoveUtil.getJumpEffect() == 0) {
-            switch (parent.offGroundTicks) {
-                case 1:
-                    mc.thePlayer.motionY = 0.39;
-                    break;
-                case 3:
-                    mc.thePlayer.motionY -= 0.13;
-                    break;
-                case 4:
-                    mc.thePlayer.motionY -= 0.1;
-                    break;
             }
         }
     }
