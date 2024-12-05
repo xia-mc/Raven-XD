b534dc4ba21cbd820b7957d9a3b1c3d6f0fdc14f
xia__mc <2052472631@qq.com>
2024-12-05 17:11:36 +0800

    Revert HypixelJump2Sprint lowhop

--- src/main/java/keystrokesmod/module/impl/world/scaffold/sprint/HypixelJump2Sprint.java
+++ src/main/java/keystrokesmod/module/impl/world/scaffold/sprint/HypixelJump2Sprint.java
@@ -2,15 +2,20 @@ package keystrokesmod.module.impl.world.scaffold.sprint;
 
 import keystrokesmod.event.PrePlayerInputEvent;
 import keystrokesmod.module.ModuleManager;
+import keystrokesmod.module.impl.exploit.disabler.hypixel.HypixelMotionDisabler;
 import keystrokesmod.module.impl.world.Scaffold;
+import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.utility.MoveUtil;
 import keystrokesmod.utility.Utils;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class HypixelJump2Sprint extends VanillaSprint {
+    private final ButtonSetting lowHop;
+
     public HypixelJump2Sprint(String name, @NotNull Scaffold parent) {
         super(name, parent);
+        this.registerSetting(lowHop = new ButtonSetting("Low hop", true));
     }
 
     @SubscribeEvent
@@ -21,6 +26,9 @@ public class HypixelJump2Sprint extends VanillaSprint {
                 MoveUtil.strafe(Math.min(MoveUtil.getAllowedHorizontalDistance(), MoveUtil.speed() * 2) - Math.random() / 100f);
                 mc.thePlayer.jump();
             }
+        } else if (parent.offGroundTicks == 5
+                && lowHop.isToggled() && HypixelMotionDisabler.isDisabled()) {
+            mc.thePlayer.motionY = MoveUtil.predictedMotion(mc.thePlayer.motionY, 2);
         }
     }
 }
