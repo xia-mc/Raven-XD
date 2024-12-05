be3ef7fceec57816ce8d35ac755df17dcf07a265
xia__mc <2052472631@qq.com>
2024-12-05 17:01:02 +0800

    Add low hop to HypixelJump2Sprint

--- src/main/java/keystrokesmod/module/impl/movement/speed/hypixel/HypixelLowHopSpeed.java
+++ src/main/java/keystrokesmod/module/impl/movement/speed/hypixel/HypixelLowHopSpeed.java
@@ -1,5 +1,6 @@
 package keystrokesmod.module.impl.movement.speed.hypixel;
 
+import keystrokesmod.module.ModuleManager;
 import keystrokesmod.module.impl.exploit.disabler.hypixel.HypixelMotionDisabler;
 import keystrokesmod.module.impl.movement.speed.HypixelSpeed;
 import keystrokesmod.module.impl.movement.speed.hypixel.lowhop.HypixelLowHopMotionSpeed;
@@ -39,6 +40,7 @@ public class HypixelLowHopSpeed extends SubMode<HypixelSpeed> {
 
     public boolean noLowHop() {
         if (!HypixelMotionDisabler.isDisabled()) return true;
+        if (ModuleManager.scaffold.isEnabled()) return true;
         return stopOnHurt.isToggled() && mc.thePlayer.hurtTime > 0;
     }
 }
