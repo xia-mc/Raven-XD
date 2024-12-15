a7b8f5aac74cd5009bf3c5d71e75879cd8130ebf
mccm9 <mccm95256@gmail.com>
2024-12-15 12:40:34 +0800

    Add Animation Camera

--- src/main/java/keystrokesmod/module/impl/render/MotionCamera.java
+++ src/main/java/keystrokesmod/module/impl/render/MotionCamera.java
@@ -24,8 +24,8 @@ public class MotionCamera extends Module {
 
     public MotionCamera() {
         super("MotionCamera", category.render);
-        this.registerSetting(offset = new SliderSetting("Offset", 0, -1, 1, 0));
-        this.registerSetting(maxOffset = new SliderSetting("Max offset", 1.5, 0, 5, 5));
+        this.registerSetting(offset = new SliderSetting("Offset", 0, -1, 1, 0.01));
+        this.registerSetting(maxOffset = new SliderSetting("Max offset", 5, 0, 5, 0.1));
         this.registerSetting(smooth = new ButtonSetting("Smooth", true));
         this.registerSetting(onlyThirdPerson = new ButtonSetting("Only third person", true));
         this.registerSetting(animationCamera = new ButtonSetting("Animation Camera", true));
